471007504a17936b160513f2e3927c191a6bfd31
Samson Ameh <amehsam69@gmail.com>
2024-03-14 23:35:38 +0100

    adds nav bar content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
 function App() {
-  return <div className="app bg-gray-20">App</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
